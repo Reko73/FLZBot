3ed303c6e30385b1a14a1d949a17a295d8efc008
Reko73 <rekogaming@hotmail.com>
2025-07-03 14:30:35 +0200

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot en ligne"
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    thread = threading.Thread(target=run)
+    thread.start()
