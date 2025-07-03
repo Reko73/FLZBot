81b9edb0b1d8067c97b8c505b3b83b4d03db9bfe
Reko73 <rekogaming@hotmail.com>
2025-07-03 20:23:11 +0200

    FallZone.py

--- FallZone.py
+++ FallZone.py
@@ -66,7 +66,7 @@ async def anonyme(interaction: discord.Interaction, contenu: str):
     draw = ImageDraw.Draw(img)
 
     try:
-        font = ImageFont.truetype("arial.ttf", 30)  # Remplace par une police RP si dispo
+        font = ImageFont.truetype("arial.ttf", 60)  # Remplace par une police RP si dispo
     except:
         font = ImageFont.load_default()
 
